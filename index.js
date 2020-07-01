9d8971796b1906fe49ffcc68b3ac5cae1c5371c4
Philip <ijeoma13a@gmail.com>
2020-06-30 17:59:26 -0700

    provisioned database for user login, with test value

--- index.js
+++ index.js
@@ -2,9 +2,37 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const { Pool } = require('pg');
+const pool = new Pool({
+   connectionString: process.env.DATABASE_URL
+});
+
+var app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/index'))
+
+/*
+  create table usr (uid char(10) NOT NULL, username char(50) NOT NULL, password char(255) NOT NULL, email char(100) NOT NULL);
+            ____________________________________________
+      usr:  |uid | username  | password  | email        |
+            --------------------------------------------
+test entry: |1   | test      | test      | test@test.com|
+            _____________________________________________
+*/
+app.get('/database', (req, res) => {
+  var getUsersQuery = `SELECT * FROM usr`;
+  pool.query(getUsersQuery, (error, result) => {
+     if (error) {
+       res.end(error);
+     }
+     var results = {'rows':result.rows};
+     res.render('pages/db', results);
+  })
+})
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
