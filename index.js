b60ff37393f9975b80569223d195ff19f3577f09
Philip <ijeoma13a@gmail.com>
2020-07-07 23:19:55 -0700

    bug fixes

--- index.js
+++ index.js
@@ -58,10 +58,10 @@ app.get('/database', (req, res) => {
 })
 
 /*
- * Respond to GET requests to /account.
- * Upon request, render the 'account.html' web page in views/ directory.
+ * Respond to GET requests to /fileUpload.
+ * Upon request, render the 'fileUpload.html' web page in views/ directory.
  */
-app.get('/account', (req, res) => res.render('fileUpload.html'));
+app.get('/fileUpload', (req, res) => res.render('fileUpload.html'));
 
 /*
  * Respond to GET requests to /sign-s3.
