af11deca733c6799706e4135a2b7b5a4a39fa5be
Philip <ijeoma13a@gmail.com>
2020-07-07 19:52:08 -0700

    Change launch page to login, and modify .json metadata

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(flash());
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-app.get('/', (req, res) => res.render('pages/index'))
+app.get('/', (req, res) => res.render('pages/login'))
 
 app.get('/database', (req, res) => {
   var getUsersQuery = `SELECT * FROM usr`;
